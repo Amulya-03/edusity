b062e4f6f0843a7ccc8727cd5db5a85662205f02
Amulya-03 <ammuluyadav100@gmail.com>
2025-12-29 13:06:00 +0530

    rebuild site

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base:'/edusity/'
-})
+})*/
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/edusity/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true
+  }
+});
+
